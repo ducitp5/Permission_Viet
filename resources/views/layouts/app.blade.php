8e576fc0de0d102a966198679de0b979ec3925e1
ASUS-PC-DUC\ADMIN <ductranp2@gmail.com>
2021-11-20 05:40:31 +0700

    BootStrap4 linked

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,6 +18,14 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    {{-- bootstrap --}}
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
 </head>
 <body>
     <div id="app">
--- routes/web.php
+++ routes/web.php
@@ -27,21 +27,24 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Route::middleware(['auth'])->group(function () {
 
     // modulle user
+
     Route::prefix('users')->group(function () {
 
         // list user
         Route::get('/', [
-            'as' => 'user.index',
-            'uses' => 'UserController@index',
+            'as'            => 'user.index',
+            'uses'          => 'UserController@index',
 //            'middleware' => 'checkacl:user-list'
         ]);
 
         // create user
+
         Route::get('/create', [
-            'as' => 'user.add',
-            'uses' => 'UserController@create',
-            'middleware' => 'checkacl:user-add'
+            'as'            => 'user.add',
+            'uses'          => 'UserController@create',
+ //           'middleware'    => 'checkacl:user-add'
         ]);
+
         Route::post('/create', 'UserController@store')->name('user.store');
         // edit user
         Route::get('/edit/{id}', 'UserController@edit')->name('user.edit');
