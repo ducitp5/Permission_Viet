54f3f84cf88b1581b480664f172cf0a3abcd9310
ASUS-PC-DUC\ADMIN <ductranp2@gmail.com>
2021-11-20 01:39:13 +0700

    $ php artisan make:controller UserController

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,34 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+// Route::get('/users', 'UserController@index');
+
+Route::middleware(['auth'])->group(function () {
+
+    // modulle user
+    Route::prefix('users')->group(function () {
+
+        // list user
+        Route::get('/', [
+            'as' => 'user.index',
+            'uses' => 'UserController@index',
+//            'middleware' => 'checkacl:user-list'
+        ]);
+
+        // create user
+        Route::get('/create', [
+            'as' => 'user.add',
+            'uses' => 'UserController@create',
+            'middleware' => 'checkacl:user-add'
+        ]);
+        Route::post('/create', 'UserController@store')->name('user.store');
+        // edit user
+        Route::get('/edit/{id}', 'UserController@edit')->name('user.edit');
+        Route::post('/edit/{id}', 'UserController@update')->name('user.edit');
+        // delete user
+        Route::get('/delete/{id}', 'UserController@delete')->name('user.delete');
+
+    });
+
+});
