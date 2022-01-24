3a8fe35074f57b9f5a6d35f6d42e9a5c453d3c04
ASUS-PC-DUC\ADMIN <ductranp2@gmail.com>
2022-01-25 02:18:09 +0700

    $ php artisan db:seed --class=PostSeeder

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Post;
 
 class PostFactory extends Factory
 {
@@ -11,10 +12,15 @@ class PostFactory extends Factory
      *
      * @return array
      */
+    protected $model   =   Post::class;
+
     public function definition()
     {
         return [
             //
+            'title'     =>  $this->faker->sentence(),
+            'body'      =>  $this->faker->text(),
+            'user_id'   =>  1,
         ];
     }
 }
